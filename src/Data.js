9446c637aa79645ddb44e9e73853dd0dcc3779fa
VenkataSriSaiSuryaMandava <ssurya.mandava@gmail.com>
2022-12-08 12:36:35 +0530

    E-Commerce

---
+++ src/Data.js
@@ -0,0 +1,40 @@
+export default {
+  users: [
+    { username: "regular", accessLevel: 1, password: "password" },
+    { username: "admin", accessLevel: 0, password: "password" }
+  ],
+  initProducts: [
+    {
+      name: "Shoes",
+      stock: 10,
+      price: 1499.99,
+      shortDesc: "Nulla facilisi. Curabitur at lacus ac velit ornare lobortis.",
+      description:
+        "Cras sagittis. Praesent nec nisl a purus blandit viverra. Ut leo. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Fusce a quam."
+    },
+    {
+      name: "Bags",
+      stock: 20,
+      price: 1569.99,
+      shortDesc: "Nulla facilisi. Curabitur at lacus ac velit ornare lobortis.",
+      description:
+        "Cras sagittis. Praesent nec nisl a purus blandit viverra. Ut leo. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Fusce a quam."
+    },
+    {
+      name: "Shirts",
+      stock: 15,
+      price: 1199.99,
+      shortDesc: "Nulla facilisi. Curabitur at lacus ac velit ornare lobortis.",
+      description:
+        "Cras sagittis. Praesent nec nisl a purus blandit viverra. Ut leo. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Fusce a quam."
+    },
+    {
+      name: "Shorts",
+      stock: 5,
+      price: 999.99,
+      shortDesc: "Nulla facilisi. Curabitur at lacus ac velit ornare lobortis.",
+      description:
+        "Cras sagittis. Praesent nec nisl a purus blandit viverra. Ut leo. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Fusce a quam."
+    }
+  ]
+};
